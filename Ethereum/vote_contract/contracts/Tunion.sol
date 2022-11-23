ed9cddb8f3989f88ff160a2e957d3124194d4559
ms3221 <hyeongjun3221@gmail.com>
2022-11-23 16:59:04 +0900

    수정

--- Ethereum/vote_contract/contracts/Tunion.sol
+++ Ethereum/vote_contract/contracts/Tunion.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.0;
 
 
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Union is ERC721URIStorage {
 
