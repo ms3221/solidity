21bb0aaf062de7111719b94029007b057aae08eb
ms3221 <hyeongjun3221@gmail.com>
2022-11-28 18:27:58 +0900

    sk Contract update 그리고 testCase 및 testCode작성

--- Klaytn/sk_sunny/contracts/SKNFT2.sol
+++ Klaytn/sk_sunny/contracts/SKNFT2.sol
@@ -9,40 +9,67 @@ import "../node_modules/@klaytn/contracts/utils/Counters.sol";
 contract SKNFT2 is KIP17URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    mapping(address => bool) whitelistedAddresses;
+    struct Member{
+        bool whitelisted;
+        uint256 badage;
+    }
+    Member public member;
+    Member[] public members;
+    mapping(address => Member) public memberByOwner;
     bool public permission;
 
-    constructor() public KIP17("SKUNK", "SK") {}
+    constructor() public KIP17("SKUNK2", "SK2") {}
 
-    modifier isWhitelisted() {
-        require(whitelistedAddresses[msg.sender], "You need to be whitelisted");
+     modifier isWhitelisted(address to) {
+        Member memory user = memberByOwner[to];
+        require(user.whitelisted, "You are not whitelisted");
         _;
     }
 
-    function changePermission() external onlyOwner {
+    function handleTransferFrom() external onlyOwner {
         permission = !permission;
     }
 
-    function addUser(address[] memory whitelist) public onlyOwner {
+    function addWhiteList(address[] memory whitelist, uint256[] calldata badge) public onlyOwner {
+
         for (uint256 i = 0; i < whitelist.length; i++) {
-            require(!whitelistedAddresses[whitelist[i]], "check your list");
-            whitelistedAddresses[whitelist[i]] = true;
+            Member storage user = memberByOwner[whitelist[i]];
+            require(!user.whitelisted, "check your list");
+            user.badage = badge[i];
+            user.whitelisted = true;
         }
+    }
+
+    function updateBadge(address to,uint256 badge) external onlyOwner {
+        Member storage user = memberByOwner[to];
+        user.badage = badge;
+    }
+
+     function handleWhiteList(address to) external onlyOwner {
+        Member storage user = memberByOwner[to];
+        user.whitelisted = !user.whitelisted;
     }
 
     function verifyUser(address _whitelistedAddress)
         public
         view
-        returns (bool)
+        returns (Member memory)
     {
-        return whitelistedAddresses[_whitelistedAddress];
+         Member memory user = memberByOwner[_whitelistedAddress];
+        return user;
     }
 
-    function mintNFT(uint badge, address to, uint tokenId, string memory tokenURI)
+    function mintNFT(address to, uint tokenId, string memory tokenURI)
         public
-        isWhitelisted
+        isWhitelisted(to)
         returns (uint256)
     {
+          Member memory user = memberByOwner[to];
+          uint256 bal = balanceOf(to);
+          uint256 badge = user.badage;
+          if(bal>0){
+            require(badge/2 > bal || badge/2 - bal >= 1, 'badge < bal or 0.5< badge < 1');
+          }
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
         return tokenId;
--- Klaytn/sk_sunny/test/sk_project_test.js
+++ Klaytn/sk_sunny/test/sk_project_test.js
@@ -2,31 +2,32 @@ const Caver = require("caver-js");
 const caver = new Caver("HTTP://127.0.0.1:7545");
 const SKNFT2 = artifacts.require("SKNFT2");
 // 명령어 truffle test ./test/sendTest.js --network gan
-contract("SKNFT", function (accounts) {
-  let walletA = accounts[0];
-  let walletB = accounts[1];
-
+contract("SKNFT2", function (accounts) {
   before(async () => {
-    SKContract = await SKNFT2.deployed();
-    console.log("SKContract", SKContract.address);
-  });
+    //SKContract = await SKNFT2.deployed();
+    SKContract = await SKNFT2.new()
+ });
+let walletA = accounts[0]
+let walletB = accounts[1]
+let walletC = accounts[2]
 
-  describe("TestCase1 : nmae / symbol", () => {
-    it("name is SKUNK", async () => {
+  describe("TestCase1 : nmae / symbol 잘나오는지 확인", () => {
+    it("name is SKUNK2", async () => {
       const name = await SKContract.name();
-      assert.equal(name, "SKUNK");
+      assert.equal(name, "SKUNK2");
     });
-    it("symbol is SK", async () => {
+    it("symbol is SK2", async () => {
       const symbol = await SKContract.symbol();
-      assert.equal(symbol, "SK");
+      assert.equal(symbol, "SK2");
     });
   });
 
   describe("TestCase2 : addUserFn / mintNFTFn", () => {
-    it("function mintNFT error발생 addUser 전", async () => {
+   
+    it("function mintNFT error발생 / whiteList 이유", async () => {
       let f = () => {};
       try {
-        await SKContract.mintNFT(1, walletA, 2, "http");
+        await SKContract.mintNFT(walletB, 1, "1");
       } catch (e) {
         f = () => {
           throw e;
@@ -35,9 +36,76 @@ contract("SKNFT", function (accounts) {
         assert.throws(f);
       }
     });
-    it("symbol is SK", async () => {
-      const symbol = await SKContract.symbol();
-      assert.equal(symbol, "SK");
+  
+    
+    it("function addWhiteList / walletB is true", async () => {
+       await SKContract.addWhiteList([walletB],[2])
+       const memberInfo = await SKContract.verifyUser(walletB)
+      assert.equal(memberInfo['whitelisted'], true);
     });
+
+    it(" wallet A is not whiteListed / walletA is false", async () => {
+      const memberInfo = await SKContract.verifyUser(walletA)
+      assert.equal(memberInfo['whitelisted'], false);
+   });
+
+   it('walletB에게 tokenId1 NFT 발행 성공 / owner, balance 비교', async () => {
+    await SKContract.mintNFT(walletB, 1, "1");
+    const bal = await SKContract.balanceOf(walletB)
+    const owner = await SKContract.ownerOf(1)
+    assert.equal(bal, 1);
+    assert.equal(owner, walletB)
+    })
   });
+  
+  describe('TestCase3. badge개수에 따른 NFT 차등 발행 test', () => {
+    before(async ()=>{
+        await SKContract.updateBadge(walletB, 5)
+        await SKContract.mintNFT( walletB, 2, "2");
+    })
+    it('badge 5개 일때 NFT는 총2개만 소유가능 3개째 mint하면 error가 발생', async () => {
+      let f = () => {};
+      try {
+        await SKContract.mintNFT(walletB, 3, "3");
+      } catch (e) {
+        f = () => {
+          throw e;
+        };
+      } finally {
+        assert.throws(f);
+      }
+    })
+    it('badge 개수를 수정하는 modifiyMemberBadge함수사용해서 badge개수를 6으로변경 3번째 NFT발행 ', async () => {
+    
+      await SKContract.updateBadge(walletB, 6)
+      await SKContract.mintNFT(walletB, 3, "3");
+      const owner = await SKContract.ownerOf(3)
+      const bal = await SKContract.balanceOf(walletB)
+      assert.equal(bal, 3);
+      assert.equal(owner, walletB)
+    })
+  })
+  describe('TestCase4. transferFrom / handleTransferFrom 확인', () => {
+    before(async ()=>{
+      await SKContract.approve(SKContract.address, 2,{from: walletB});
+  })
+    it('handleTransferFrom 함수 실행 전 error가 발생해야 정상', async () => {
+      let f = () => {};
+      try {
+        await SKContract.transferFrom(walletB, walletA, 2,{from :walletB});
+      } catch (e) {
+        f = () => {
+          throw e;
+        };
+      } finally {
+        assert.throws(f);
+      }
+    })
+    it('handleTransferFrom 함수를 실행하고 나면 TransferFrom 함수가 정상적으로 작동하게 된다. ', async () => {
+      await SKContract.handleTransferFrom();
+      await SKContract.transferFrom(walletB, walletA, 2,{from :walletB});
+      const owner = await SKContract.ownerOf(2)
+      assert.equal(owner, walletA)
+    })
+  })
 });
